ebeb0e2a6db5292aada72d1aa2000ba6d354b48f
Kaio <kakacnmendes@gmail.com>
2024-10-08 18:54:37 -0300

    Home

--- poke-api/src/routes/Home.tsx
+++ poke-api/src/routes/Home.tsx
@@ -14,7 +14,6 @@ const Home = () => {
     return (
             <Container>
                 <Header />
-                <SearchBar />
                 <PokemonList />
             </Container>
     )
